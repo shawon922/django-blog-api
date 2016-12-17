2f553fe2a96532b270ac984fe848507e6112d866
Md Mahfuzur Rahman <shawon922@gmail.com>
2016-12-17 23:15:49 +0600

    api for posts app done

--- posts/api/serializers.py
+++ posts/api/serializers.py
@@ -1,4 +1,8 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import (
+    ModelSerializer,
+    HyperlinkedIdentityField,
+    SerializerMethodField,
+)
 
 from posts.models import Post
 
@@ -13,23 +17,61 @@ class PostCreateUpdateSerializer(ModelSerializer):
 
 
 class PostListSerializer(ModelSerializer):
+    url = HyperlinkedIdentityField(
+        view_name='posts-api:detail',
+        lookup_field='slug'
+    )
+    # delete_url = HyperlinkedIdentityField(
+    #     view_name='posts-api:delete',
+    #     lookup_field='slug'
+    # )
+
+    user = SerializerMethodField()
+    image = SerializerMethodField()
+
     class Meta:
         model = Post
         fields = [
+            'url',
+            'user',
             'title',
-            'slug',
             'content',
             'timestamp',
+            'image',
         ]
+
+    def get_user(self, obj):
+        return str(obj.user.username)
+
+    def get_image(self, obj):
+        try:
+            image = obj.image.url
+        except:
+            image = None
+        return image
 
 
 class PostDetailSerializer(ModelSerializer):
+    user = SerializerMethodField()
+    image = SerializerMethodField()
+
     class Meta:
         model = Post
         fields = [
-            'id',
+            'user',
             'title',
             'slug',
             'content',
             'timestamp',
+            'image',
         ]
+
+    def get_user(self, obj):
+        return str(obj.user.username)
+
+    def get_image(self, obj):
+        try:
+            image = obj.image.url
+        except:
+            image = None
+        return image
--- posts/api/views.py
+++ posts/api/views.py
@@ -1,12 +1,36 @@
-from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
+from django.db.models import Q
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, RetrieveUpdateAPIView, RetrieveDestroyAPIView
+from rest_framework.filters import (
+    SearchFilter,
+    OrderingFilter,
+)
+
+from rest_framework.permissions import (
+    AllowAny,
+    IsAuthenticated,
+    IsAdminUser,
+    IsAuthenticatedOrReadOnly,
+)
 
 from posts.models import Post
+
+from .pagination import (
+    PostLimitOffsetPagination,
+    PostPageNumberPagination,
+)
+from .permissions import (
+    IsOwnerOrReadOnly,
+)
 from .serializers import PostCreateUpdateSerializer, PostListSerializer, PostDetailSerializer
 
 
 class PostCreateAPIView(CreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostCreateUpdateSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
 
 
 class PostDetailAPIView(RetrieveAPIView):
@@ -16,19 +40,42 @@ class PostDetailAPIView(RetrieveAPIView):
     # lookup_url_kwarg = 'abc'
 
 
-class PostUpdateAPIView(UpdateAPIView):
+class PostUpdateAPIView(RetrieveUpdateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostCreateUpdateSerializer
     lookup_field = 'slug'
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+    def perform_update(self, serializer):
+        serializer.save(user=self.request.user)
 
 
-class PostDeleteAPIView(DestroyAPIView):
+class PostDeleteAPIView(RetrieveDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostDetailSerializer
     lookup_field = 'slug'
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
 
 class PostListAPIView(ListAPIView):
-    queryset = Post.objects.all()
+    # queryset = Post.objects.all()
+    filter_backends = [SearchFilter, OrderingFilter]
+    search_fields = ['title', 'content', 'user__username', 'user__first_name', 'user__last_name']
     serializer_class = PostListSerializer
+    pagination_class = PostPageNumberPagination # PostLimitOffsetPagination # PageNumberPagination
+
+    def get_queryset(self, *args, **kwargs):
+        # queryset_list = super(PostListAPIView, self).get_queryset(*args, **kwargs)
+        queryset_list = Post.objects.all()
+        query = self.request.GET.get('q')
+        if query:
+            queryset_list = queryset_list.filter(
+                Q(title__icontains=query)|
+                Q(content__icontains=query)|
+                Q(user__username__icontains=query)|
+                Q(user__first_name__icontains=query)|
+                Q(user__last_name__icontains=query)
+            ).distinct()
+
+        return queryset_list
 
